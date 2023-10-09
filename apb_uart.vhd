177be7e7d0d697c0275c28eaaf792f6d9239900a
Victor O. Costa <victor-o.c@hotmail.com>
2023-10-08 21:37:07 -0300

    Implement memory-mapped control register (1-bit) and a configurable number of stop bits (1 or 2)

--- apb_uart.vhd
+++ apb_uart.vhd
@@ -22,10 +22,18 @@ entity apb_uart is
 		-- Bus widths
 		APB_DATA_WIDTH   : natural range 0 to 32 := APB_DATA_WIDTH_c;     -- Width of the APB data bus
 		APB_ADDR_WIDTH   : natural range 0 to 32 := APB_ADDR_WIDTH_c;     -- Width of the address bus
-		UART_DATA_WIDTH  : natural range 0 to 32 := UART_DATA_WIDTH_c;    -- Width of the APB data bus
-		UART_FBAUD_WIDTH : natural range 0 to 32 := UART_FBAUD_WIDTH_c;   -- Width of the address bus
-		-- Reset value for the frequency/baud register
-		UART_FBAUD_RSTVL : natural range 0 to UART_FBAUD_WIDTH_c**2 - 1 := UART_FBAUD_SIM_c
+		-- Memory-mapped registers
+		-- Register widths
+		UART_DATA_WIDTH  : natural range 0 to 32 := UART_DATA_WIDTH_c;    -- Width of the UART words
+		UART_CTRL_WIDTH  : natural range 0 to 32 := UART_CTRL_WIDTH_c;    -- Width of the FBAUD register
+		UART_FBAUD_WIDTH : natural range 0 to 32 := UART_FBAUD_WIDTH_c;   -- Width of the FBAUD register
+		-- Register addresses
+		UART_DATA_ADDR   : std_logic_vector(APB_ADDR_WIDTH_c-1 downto 0)  := UART_DATA_ADDR_c;
+		UART_CTRL_ADDR   : std_logic_vector(APB_ADDR_WIDTH_c-1 downto 0)  := UART_CTRL_ADDR_c;
+		UART_BAUD_ADDR   : std_logic_vector(APB_ADDR_WIDTH_c-1 downto 0)  := UART_BAUD_ADDR_c;
+		-- Register reset values
+		UART_FBAUD_RSTVL : std_logic_vector(UART_FBAUD_WIDTH_c-1 downto 0)  := UART_FBAUD_SIM_c;
+		UART_CTRL_RSTVL  : std_logic_vector(UART_CTRL_WIDTH_c-1 downto 0) := UART_CTRL_RSTVL_c
 	);
 	port(
 		-- Clock and negated reset
@@ -63,15 +71,19 @@ architecture behavioral of apb_uart is
 	-- Memory-mapped registers
 	-- Addresses are aligned with 32-bit words 
 	-- 0x00 - UART data transmission register (8 bits)
-	-- 0x04 - UART frequency/baud ratio register: threshold for clock counter (16 bits) - floor(clk_freq/baud_rate)
-	-- 0x08 (future) UART status register
-	-- 0x0C (future) UART configuration register
+	-- 0x04 - UART control register (1 bit)
+	---- [0] - Stop bit: LOW for one, high for TWO stop bits
+	-- 0x08 - UART frequency/baud ratio register: threshold for clock counter (16 bits, configurable) - floor(clk_freq/baud_rate)
+	-- 0x0C (future) UART status register
 	signal reg_data_tx    : std_logic_vector(UART_DATA_WIDTH-1 downto 0);
 	signal reg_data_rx    : std_logic_vector(UART_DATA_WIDTH-1 downto 0);
+	signal reg_control    : std_logic_vector(UART_CTRL_WIDTH-1 downto 0);
 	signal reg_fbaud      : std_logic_vector(UART_FBAUD_WIDTH-1 downto 0);
 	-- Associated signals
-	signal uart_data_rx_s : std_logic_vector(APB_DATA_WIDTH-1 downto 0);
-	signal uart_baud_s    : std_logic_vector(APB_ADDR_WIDTH-1 downto 0);
+	signal uart_data_rx_s   : std_logic_vector(APB_DATA_WIDTH-1 downto 0);
+	signal uart_baud_s      : std_logic_vector(APB_ADDR_WIDTH-1 downto 0);
+	-- Control register outline
+	signal uart_ctrl_stop_s : std_logic;
 	
 	-- UART
 	-- Tx and Rx finite state machines
@@ -80,8 +92,8 @@ architecture behavioral of apb_uart is
 	signal reg_state_rx   : fsm_state_uart_t;
 	
 	-- Clock counters for baud generation
-	signal reg_clk_count_tx : integer range 0 to 2**UART_FBAUD_WIDTH - 1;
-	signal reg_clk_count_rx : integer range 0 to 2**UART_FBAUD_WIDTH - 1;
+	signal reg_clk_count_tx : integer range 0 to 2**(UART_FBAUD_WIDTH + 1) - 1;
+	signal reg_clk_count_rx : integer range 0 to 2**(UART_FBAUD_WIDTH + 1) - 1;
 	
 	-- Shift shift registers which drive reg_data_tx and reg_data_rx
 	signal reg_shift_tx : std_logic_vector(UART_DATA_WIDTH-1 downto 0);
@@ -104,7 +116,8 @@ begin
 			if rstn = '0' then
 				-- Memory-mapped registers driven by APB
 				reg_data_tx <= (others => '0');
-				reg_fbaud <= std_logic_vector(to_unsigned(UART_FBAUD_RSTVL, reg_fbaud'length));
+				reg_control <= UART_CTRL_RSTVL;
+				reg_fbaud   <= UART_FBAUD_RSTVL;
 				-- State register
 				reg_state_apb <= Sapb_idle;
 			else
@@ -128,10 +141,13 @@ begin
 					
 						-- APB writes to memory-mapped registers
 						-- UART data Tx register
-						if (penable_i and pwrite_i) = '1' and paddr_i = UART_DATA_ADDR_c then
+						if (penable_i and pwrite_i) = '1' and paddr_i = UART_DATA_ADDR then
 							reg_data_tx <= pwdata_i(UART_DATA_WIDTH-1 downto 0);
+						-- UART control register
+						elsif (penable_i and pwrite_i) = '1' and paddr_i = UART_CTRL_ADDR then
+							reg_control <= pwdata_i(UART_CTRL_WIDTH-1 downto 0);
 						-- UART frequency/baud ratio register
-						elsif (penable_i and pwrite_i) = '1' and paddr_i = UART_BAUD_ADDR_c then
+						elsif (penable_i and pwrite_i) = '1' and paddr_i = UART_BAUD_ADDR then
 							reg_fbaud <= pwdata_i(UART_FBAUD_WIDTH-1 downto 0);
 						end if;
 							
@@ -149,7 +165,7 @@ begin
 	end process;
 	
 	-- UART Rx FSM
-	-- Samples at the middle of the interval;
+	-- Samples the Rx signal;
 	-- FUTURE: sample multiple times and do a voting
 	rx_sampling_s <= '1' when reg_clk_count_rx = to_integer(unsigned(reg_fbaud))/2 else '0';
 
@@ -267,7 +283,7 @@ begin
 						reg_bit_count_tx <= 0;
 						
 						-- UART Tx start depends on the APB frontend
-						if (penable_i and psel_i and pwrite_i) = '1' and paddr_i = UART_DATA_ADDR_c then
+						if (penable_i and psel_i and pwrite_i) = '1' and paddr_i = UART_DATA_ADDR then
 							reg_state_tx <= Suart_start_bit;
 						else 
 							reg_state_tx <= Suart_idle;
@@ -312,21 +328,43 @@ begin
 
 					-- UART TX STOP_BIT STATE
 					when Suart_stop_bit =>
-						-- Clock counter increment and reset
-						if reg_clk_count_tx = to_integer(unsigned(reg_fbaud)) - 1 then
-							-- Next state
-							reg_state_tx <= Suart_idle;
-							reg_clk_count_tx <= 0;
-						else
-							-- Next state
-							reg_state_tx <= Suart_stop_bit;
-							reg_clk_count_tx <= reg_clk_count_tx + 1;
+						-- 1 stop bit
+						if uart_ctrl_stop_s = '0' then
+							-- Clock counter increment and reset
+							if reg_clk_count_tx = to_integer(unsigned(reg_fbaud)) - 1 then
+								-- Next state
+								reg_state_tx <= Suart_idle;
+								reg_clk_count_tx <= 0;
+							else
+								-- Next state
+								reg_state_tx <= Suart_stop_bit;
+								reg_clk_count_tx <= reg_clk_count_tx + 1;
+							end if;
+						
+						-- 2 stop bits
+						else 
+							-- Clock counter increment and reset
+							if reg_clk_count_tx = (2 * to_integer(unsigned(reg_fbaud))) - 1 then
+								-- Next state
+								reg_state_tx <= Suart_idle;
+								reg_clk_count_tx <= 0;
+							else
+								-- Next state
+								reg_state_tx <= Suart_stop_bit;
+								reg_clk_count_tx <= reg_clk_count_tx + 1;
+							end if;
+						
 						end if;
+						
+
 						
 				end case;
 			end if;
 		end if;
 	end process;
+
+	-- Control register signals
+	uart_ctrl_stop_s    <= reg_control(0);	
 
 	-- Drive APB outputs
 	-- Assembly APB-width signals
@@ -335,16 +373,16 @@ begin
 	uart_baud_s(APB_DATA_WIDTH-1 downto UART_FBAUD_WIDTH_c)   <= (others => '0');
 	uart_baud_s(UART_FBAUD_WIDTH-1 downto 0)                  <= reg_fbaud;
 	
-	prdata_o  <= uart_data_rx_s when reg_state_apb = Sapb_setup and pwrite_i = '0' and reg_paddr = UART_DATA_ADDR_c else
-	             uart_baud_s    when reg_state_apb = Sapb_setup and pwrite_i = '0' and reg_paddr = UART_BAUD_ADDR_c else
+	prdata_o  <= uart_data_rx_s when reg_state_apb = Sapb_setup and pwrite_i = '0' and reg_paddr = UART_DATA_ADDR else
+	             uart_baud_s    when reg_state_apb = Sapb_setup and pwrite_i = '0' and reg_paddr = UART_BAUD_ADDR else
 	             (others => '0');
   
 	-- Peripherals with fixed 2-cycle access can tie PREADY high
 	pready_o <= '1';
 	
 	-- 
-	address_exists_s <= '1' when reg_state_apb = Sapb_setup and (reg_paddr = UART_BAUD_ADDR_c or 
-	                                                             reg_paddr = UART_DATA_ADDR_c) else '0';
+	address_exists_s <= '1' when reg_state_apb = Sapb_setup and (reg_paddr = UART_BAUD_ADDR or 
+	                                                             reg_paddr = UART_DATA_ADDR) else '0';
 	pslverr_o        <= '1' when reg_state_apb = Sapb_setup and address_exists_s = '0' else '0';
 	
 	-- Drive UART
@@ -357,6 +395,8 @@ begin
 	-- 0: Rx interrupt is set during a single cycle, indicating that there is a byte available at Rx
 	-- 1: Tx interrupt is set during a single cycle, indicating the end of a transmission.
 	int_o(0) <= rx_sampling_s when reg_state_rx = Suart_stop_bit else '0';
-	int_o(1) <= '1' when reg_state_tx = Suart_stop_bit and reg_clk_count_tx = to_integer(unsigned(reg_fbaud)) - 1 else '0'; 
+	int_o(1) <= '1' when (reg_state_tx = Suart_stop_bit and uart_ctrl_stop_s = '0' and reg_clk_count_tx = to_integer(unsigned(reg_fbaud)) - 1) or
+                       (reg_state_tx = Suart_stop_bit and uart_ctrl_stop_s = '1' and reg_clk_count_tx = (2 * to_integer(unsigned(reg_fbaud))) - 1) else
+							'0'; 
 
 end behavioral;
--- uart_constants_pkg.vhd
+++ uart_constants_pkg.vhd
@@ -23,21 +23,25 @@ package uart_constants_pkg is
 	-- Peripheral interrupt width
 	constant PERIPH_INT_WIDTH_c : natural := 2;
 	
-	-- Memory-mapped register widths
-	constant UART_DATA_WIDTH_c  : natural := 8;
-	constant UART_FBAUD_WIDTH_c : natural := 16;
-	-- Frequency/baud value for simulation
-	constant UART_FBAUD_SIM_c   : natural := 4;                           
-	
-	-- Memory-mapped register addresses
+	-- Memory-mapped registers 
 	-- Addresses are aligned with 32-bit words 
 	-- 0x00 - UART data transmission register (8 bits)
-	-- 0x04 - UART frequency/baud ratio register: threshold for clock counter (16 bits) - floor(clk_freq/baud_rate)
-	-- 0x08 (future) UART status register
-	-- 0x0C (future) UART configuration register
-	constant UART_DATA_ADDR_c : std_logic_vector(APB_ADDR_WIDTH_c-1 downto 0) := x"00000000";
-	constant UART_BAUD_ADDR_c : std_logic_vector(APB_ADDR_WIDTH_c-1 downto 0) := x"00000004";
-
+	-- 0x04 - UART control register (1 bit)
+	---- [0] - Stop bit: LOW for one, high for TWO stop bits
+	-- 0x08 - UART frequency/baud ratio register: threshold for clock counter (16 bits, configurable) - floor(clk_freq/baud_rate)
+	-- 0x0C (future) UART status register
+	-- Register widths
+	constant UART_DATA_WIDTH_c  : natural := 8;
+	constant UART_CTRL_WIDTH_c  : natural := 1;
+	constant UART_FBAUD_WIDTH_c : natural := 16;
+	-- Register addresses
+	constant UART_DATA_ADDR_c   : std_logic_vector(APB_ADDR_WIDTH_c-1 downto 0) := x"00000000";
+	constant UART_CTRL_ADDR_c   : std_logic_vector(APB_ADDR_WIDTH_c-1 downto 0) := x"00000004";
+	constant UART_BAUD_ADDR_c   : std_logic_vector(APB_ADDR_WIDTH_c-1 downto 0) := x"00000008";
+	-- Register reset values
+	constant UART_FBAUD_SIM_c   : std_logic_vector(UART_FBAUD_WIDTH_c-1 downto 0) := x"0003";
+	constant UART_CTRL_RSTVL_c  : std_logic_vector(UART_CTRL_WIDTH_c-1 downto 0) := "0"; 
+	
 end package uart_constants_pkg;
 
 
